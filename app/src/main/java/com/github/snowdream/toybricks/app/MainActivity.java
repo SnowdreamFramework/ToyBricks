d85999743868d7f33ea025c7deef5b8bc0fdb2bf
snowdream <yanghui1986527@gmail.com>
2017-02-19 17:18:36 +0800

    ADD: kotlin demo

--- app/src/main/java/com/github/snowdream/toybricks/app/MainActivity.java
+++ app/src/main/java/com/github/snowdream/toybricks/app/MainActivity.java
@@ -3,6 +3,7 @@ package com.github.snowdream.toybricks.app;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.TextView;
+import com.github.snowdream.kotlin.helloworld.IKotlinText;
 import com.github.snowdream.toybricks.ToyBricks;
 
 public class MainActivity extends AppCompatActivity {
@@ -13,7 +14,13 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         IText text = ToyBricks.getImplementation(IText.class);
+        IKotlinText ktext = ToyBricks.getImplementation(IKotlinText.class);
 
-        ((TextView)findViewById(R.id.text)).setText(text.getText());
+        StringBuilder builder = new StringBuilder();
+        builder.append(text.getText());
+        builder.append("\n\n\n");
+        builder.append(ktext.getText());
+
+        ((TextView)findViewById(R.id.text)).setText(builder.toString());
     }
 }
---
+++ app/src/main/kotlin/com/github/snowdream/toybricks/app/IKotlinText.kt
@@ -0,0 +1,12 @@
+package com.github.snowdream.kotlin.helloworld
+
+import com.github.snowdream.toybricks.annotation.Interface
+
+/**
+ * Created by snowdream on 17/2/11.
+ */
+@Interface
+interface IKotlinText {
+
+    val text: String
+}
