d0b3f638f9aca093b1bd191fbb7a1c6254ea0ba6
snowdream <yanghui1986527@gmail.com>
2017-04-07 23:02:29 +0800

    Release: 0.9.4

--- plugin/src/main/groovy/com/github/snowdream/toybricks/plugin/ToybricksPlugin.groovy
+++ plugin/src/main/groovy/com/github/snowdream/toybricks/plugin/ToybricksPlugin.groovy
@@ -3,6 +3,7 @@ package com.github.snowdream.toybricks.plugin
 import org.gradle.api.Plugin
 import org.gradle.api.Project
 import org.gradle.api.Task
+import org.gradle.api.tasks.Copy
 import org.gradle.api.tasks.StopExecutionException
 import org.gradle.api.tasks.compile.JavaCompile
 
@@ -16,30 +17,94 @@ class ToybricksPlugin implements Plugin<Project> {
     private static final String PLUGIN_ANDROID_LIBRARY = "com.android.library"
     private static final String PLUGIN_ANDROID_KOTLIN = "kotlin-android"
 
+    private static final int CONFIGURATION_TYPE_APT = 0 //java
+    private static final int CONFIGURATION_TYPE_KAPT = 1 //kotlin
+
+
     private Project project
+
+    private int type = CONFIGURATION_TYPE_APT
 
     void apply(Project project) {
         this.project = project
 
         checkEnvironment()
 
+        checkConfigurationType()
+
+        if (hasPlugin(PLUGIN_ANDROID_APP)){
+            createAndroidAppTasks()
+        }
+
+        if (hasPlugin(PLUGIN_ANDROID_LIBRARY)) {
+            createAndroidLibraryTasks()
+        }
+    }
+
+    /**
+     * check environment
+     */
+    def checkEnvironment() {
+        if (!hasPlugin(PLUGIN_ANDROID_APP) && !hasPlugin(PLUGIN_ANDROID_LIBRARY) ) {
+            throw new StopExecutionException(
+                    "Must be applied after 'com.android.application' or 'com.android.library' plugin.")
+        }
+    }
+
+    /**
+     * check configuration type
+     */
+    def checkConfigurationType() {
+        if (!project.configurations.annotationProcessor.isEmpty()){
+            type = CONFIGURATION_TYPE_APT
+        }else if (!project.configurations.kapt.isEmpty()){
+            type = CONFIGURATION_TYPE_KAPT
+        }else {
+            type = CONFIGURATION_TYPE_APT
+        }
+    }
+
+    /**
+     * hasPlugin
+     *
+     * @param name plugin name
+     * @return
+     */
+    private boolean hasPlugin(String name) {
+        return project.plugins.hasPlugin(name)
+    }
+
+    /**
+     * create tasks for android app
+     *
+     * @param project
+     * @return
+     */
+    def createAndroidAppTasks() {
         project.android.applicationVariants.all { variant ->
             def variantCapitalizeName = "${variant.name}".capitalize()
 
-            def aptOutputDir = project.file("build/generated/source/apt")
-            def aptOutput = new File(aptOutputDir, variant.dirName)
-            project.android.sourceSets[getSourceSetName(variant)].java.srcDirs += aptOutput.getPath()
+            //see: https://bitbucket.org/qbusict/android-gradle-scripts/src/60394ba8f5efa64badb17c9388f38b89f09c5d9a/annotations.groovy
+            def aptOutputDir
+            if (type == CONFIGURATION_TYPE_KAPT) {
+                aptOutputDir = project.file(new File("${project.buildDir}", "generated/source/kapt/${variant.name}"))
+            }else{
+                aptOutputDir = project.file(new File("${project.buildDir}", "generated/source/apt/${variant.dirName}"))
+            }
+
+            project.android.sourceSets["${variant.name}"].java.srcDirs += aptOutputDir.getPath()
 
             project.task("process${variantCapitalizeName}ToyBricksJsonFile", type: ToybricksTask) {
                 description "Merge ToyBricks.json from dependencies into one, Then generate java source file: InterfaceLoaderImpl.java."
 
                 dependencies = project.configurations.compile
+
                 isKotlinAndroidProject = hasPlugin(PLUGIN_ANDROID_KOTLIN)
-                if (isKotlinAndroidProject) {
-                    outputDirs = project.files(new File("${project.buildDir}", "generated/source/kapt/${variant.name}"),
-                            new File("${project.buildDir}", "generated/source/apt/${variant.name}"))
-                } else {
+
+                if (type == CONFIGURATION_TYPE_KAPT) {
                     outputDirs = project.files(new File("${project.buildDir}", "generated/source/kapt/${variant.name}"))
+                }else{
+                    outputDirs = project.files(new File("${project.buildDir}", "generated/source/apt/${variant.dirName}"))
                 }
             }
 
@@ -62,7 +127,7 @@ class ToybricksPlugin implements Plugin<Project> {
                     includes = javaCompileTask.getIncludes()
                     excludes = javaCompileTask.getExcludes()
                     //source = variant.javaCompile.source
-                    source = aptOutput.getPath()
+                    source = aptOutputDir.getPath()
                 }
 
                 project.tasks["compile${variantCapitalizeName}ToyBricksGeneratedSources"].dependsOn(project.tasks["process${variantCapitalizeName}ToyBricksJsonFile"])
@@ -80,24 +145,26 @@ class ToybricksPlugin implements Plugin<Project> {
         }
     }
 
-    def checkEnvironment() {
-        if (!hasPlugin(PLUGIN_ANDROID_APP)) {
-            throw new StopExecutionException(
-                    "Must be applied after 'android' plugin.")
-        }
-    }
-
     /**
-     * hasPlugin
+     * create tasks for android library
      *
-     * @param name plugin name
+     * @param project
      * @return
      */
-    private boolean hasPlugin(String name) {
-        return project.plugins.hasPlugin(name)
-    }
+    def createAndroidLibraryTasks() {
+        project.android.libraryVariants.all { variant ->
+            def variantCapitalizeName = "${variant.name}".capitalize()
 
-    private String getSourceSetName(def variant) {
-        return new File(variant.dirName).getName();
+            project.task("copy${variantCapitalizeName}ToyBricksJsonFile", type: Copy) {
+                if (type == CONFIGURATION_TYPE_KAPT){
+                    from "${project.buildDir}/generated/source/kapt/${variant.name}"
+                }else {
+                    from "${project.buildDir}/generated/source/apt/${variant.name}"
+                }
+                into "${project.buildDir}/intermediates/bundles/${variant.dirName}"
+            }
+            project.tasks["copy${variantCapitalizeName}ToyBricksJsonFile"].dependsOn(project.tasks["extract${variantCapitalizeName}Annotations"])
+            project.tasks["extract${variantCapitalizeName}Annotations"].finalizedBy(project.tasks["copy${variantCapitalizeName}ToyBricksJsonFile"])
+        }
     }
 }
