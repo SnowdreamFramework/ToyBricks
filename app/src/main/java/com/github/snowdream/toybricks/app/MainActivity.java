28234e970bbe9cd3c0284320a6bfb5118ced3315
snowdream <yanghui1986527@gmail.com>
2017-04-09 15:51:40 +0800

    Release: 0.9.5

--- app/src/main/java/com/github/snowdream/toybricks/app/MainActivity.java
+++ app/src/main/java/com/github/snowdream/toybricks/app/MainActivity.java
@@ -3,7 +3,6 @@ package com.github.snowdream.toybricks.app;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.TextView;
-import com.github.snowdream.kotlin.helloworld.IKotlinText;
 import com.github.snowdream.toybricks.ToyBricks;
 
 public class MainActivity extends AppCompatActivity {
@@ -14,12 +13,9 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         IText text = ToyBricks.getImplementation(IText.class);
-        IKotlinText ktext = ToyBricks.getImplementation(IKotlinText.class);
 
         StringBuilder builder = new StringBuilder();
         builder.append(text.getText());
-        builder.append("\n\n\n");
-        builder.append(ktext.getText());
 
         ((TextView)findViewById(R.id.text)).setText(builder.toString());
     }
